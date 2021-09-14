ae39e2a0aa9e5b34de079000c6f75d46b0be5d2a
Carlos-Laparra <70443027+Carlos-Laparra@users.noreply.github.com>
2021-09-13 23:11:41 -0600

    Correciones

--- Solucion 2/HT/Program.cs
+++ Solucion 2/HT/Program.cs
@@ -1,29 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace ConsoleApp1
+namespace HT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Escriba mensaje a comprimir:");
-            string mensaje = Console.ReadLine();
-
-
+            Console.WriteLine("Huffman Test");
+            Console.WriteLine("Escriba el mensaje a comprimir:");
+            string mensaje = "";
             Libreria_ED_II_F.HuffmanFinal.Comprimir MHFC = new Libreria_ED_II_F.HuffmanFinal.Comprimir(mensaje);
+
             Console.WriteLine("El mensaje comprimido es:");
             Console.WriteLine(MHFC.mensajeComprimido);
 
-            Console.Read();
-            Console.ReadKey();
-            Console.WriteLine("El mensaje descomprimido es:");
+            Console.Clear();
+
+            Console.WriteLine("El mensaje original es:");
             Libreria_ED_II_F.HuffmanFinal.Descomprimir MHFD = new Libreria_ED_II_F.HuffmanFinal.Descomprimir(MHFC.mensajeComprimido);
             Console.WriteLine(MHFD.mensajeDescomprimido);
-            
-            
-            
         }
     }
 }
