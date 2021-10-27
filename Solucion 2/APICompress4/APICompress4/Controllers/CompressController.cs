1e0bcb43a6d7132db7cebd24e1a92464a0fb3a33
martinbechfurlan <martinbechfurlan@gmail.com>
2021-10-27 11:05:59 -0600

    final huf

---
+++ Solucion 2/APICompress4/APICompress4/Controllers/CompressController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using APICompress4.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Compress;
+
+namespace APICompress4.Controllers
+{
+    [Route("api/compress")]
+    [ApiController]
+    public class CompressController : CompressIController
+    {
+        
+        
+
+        [HttpPost("{name}")]
+        public ActionResult<string> compress ([FromForm]List<IFormFile> files, string name )
+        {
+            string path = Path.Combine("../..", "Archivos");
+            string newFileName = "f";
+            IFormFile file = files[0];
+            try
+            {
+                
+                byte[] fileBytes = null;
+                byte[] filebytesantes = null;
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    fileBytes = ms.ToArray();
+                    filebytesantes = ms.ToArray();
+                    HuffmanCompress x = new HuffmanCompress();
+                    fileBytes = x.compress(fileBytes);
+                }
+                saveFileAfter(fileBytes, path, name + ".huff");
+                double SrazonDeCompresion = Convert.ToDouble(Convert.ToDouble(fileBytes.Length) / Convert.ToDouble(filebytesantes.Length) * 100);
+                double SfactorDeCompresion = Convert.ToDouble(100 / SrazonDeCompresion);
+                double SporcentajeDeReduccion = Convert.ToDouble(100 - SrazonDeCompresion);
+
+                Compression compress = new Compression(file.FileName, Path.Combine(path, newFileName), SrazonDeCompresion, SfactorDeCompresion, SporcentajeDeReduccion);
+                uploadedFiles.Add(compress);
+
+            }
+            catch(Exception e)
+            {
+                StatusCodeResult x = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                return x;
+            }
+
+            return file.FileName;
+        }
+
+        private string saveFile(IFormFile file, string path)
+        {
+            string fileName = Path.GetFileName(file.FileName);
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);
+                //uploadedFiles.Add(fileName);
+            }
+            return Path.Combine(path, fileName);
+        }
+
+        private void saveFileAfter(byte[] fileData, string path, string fileName)
+        {
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                stream.Write(fileData, 0, fileData.Length);
+                //uploadedFiles.Add(fileName);
+            }
+        }
+    }
+}
---
+++ Solucion 2/Compress/Compress/Compress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compress
+{
+    public interface Compresss
+    {
+        byte[] compress(byte[] data);
+
+        byte[] descompress(byte[] data);
+    }
+}
